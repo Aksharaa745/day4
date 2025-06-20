d620e277ed0015400f9b92a9428666789c740c3a
Aksharaa745 <aksharaab73@gmail.com>
2025-06-20 11:47:37 +0530

    complete script topic

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// let newObject = {
-//     name: "John",
-//     display: function(){
-//         console.log("Hello");
-//     }
+let newObject = {
+    name: "John",
+    display: function(){
+        console.log("Hello");
+    }
 
-// }
-// newObject.display();
+}
+newObject.display();
 
-// console.log("Hello js!");
-// console.error("Something went wrong!");
-// console.warn("This is a warning");
-// console.table([{name: "Alic"}, {name: "Bob"}]);
-// console.time("loop");
-// for(let i =0;i<100000;i++);
-// console.timeEnd("loop");
+console.log("Hello js!");
+console.error("Something went wrong!");
+console.warn("This is a warning");
+console.table([{name: "Alic"}, {name: "Bob"}]);
+console.time("loop");
+for(let i =0;i<100000;i++);
+console.timeEnd("loop");
 
-// //eval() function - don't use this in live projects
+//eval() function - don't use this in live projects
 
-// const x =10;
-// const y = 20;
-// const result = eval("x + y");
-// console.log(result);
+const x =10;
+const y = 20;
+const result = eval("x + y");
+console.log(result);
 
 
 //strict mode
 
-// "use strict";
-// let a = 10;
-// b=20;
+"use strict";
+let a = 10;
+b=20;
